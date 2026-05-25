12d2f2de1ee66cfeeccf90711dda1120727c43c1
@pavan.teja <pavan.teja@knolskape.com>
2026-05-26 01:41:55 +0530

    Add audio upload middleware to transcribeAudio route

--- backend/src/routes/sarvam.routes.ts
+++ backend/src/routes/sarvam.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { transcribeAudioController } from "../controllers/sarvam.controller.js";
+import { uploadAudio } from "../config/multer.config.js";
 
 export const sarvamRouter = Router();
 
 
 sarvamRouter.route('/transcribeAudio')
-    .post(transcribeAudioController)
+    .post(uploadAudio.single('audio'), transcribeAudioController);
